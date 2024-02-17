ca1ea7ea9749f95dc18e34cc31a549a72d0a42c8
kiriglor <68651006+kiriglor@users.noreply.github.com>
2024-02-17 06:19:19 +0500

    Update show.py

--- app/show.py
+++ app/show.py
@@ -1,10 +1,10 @@
 import argparse
 
 def show(id):
-    with open('data/'+id+'.txt','r') as file:
+    with open('data/'+id+'.txt', 'r') as file:
         print(file.read())
 
-parser = argparse.ArgumentParser(prog='Интерфейс для поиска записей')
+parser = argparse.ArgumentParser(prog='Интерфейс для вывода записи на экран')
 parser.add_argument('id', type=str, help='id объекта')
 arg = parser.parse_args().id
 show(arg)
