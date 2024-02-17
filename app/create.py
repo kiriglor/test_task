d3fe8eb9c1a25ae952d71a8ad7e5530537163b6e
kiriglor <68651006+kiriglor@users.noreply.github.com>
2024-02-17 06:12:10 +0500

    Update create.py

--- app/create.py
+++ app/create.py
@@ -2,6 +2,9 @@ import datetime
 
 
 def create():
+    '''
+    Функция для создания текстовой записи
+    '''
     lst = {'Фамилия': False,
            'Имя': False,
            'Отчество': False,
@@ -11,14 +14,13 @@ def create():
 
     for title in lst.keys():
         lst[title] = input(title + ': ')
-
     date_time = datetime.datetime.now()
     format = '%Y-%m-%d-%H-%M'
     string = date_time.strftime(format)
-
-    with open('data/id.txt','r') as id:
+    
+    with open('data/id.txt', 'r') as id:
         file_id = id.read()
-    with open('data/id.txt','w') as id:
+    with open('data/id.txt', 'w') as id:
         id.write(str(int(file_id)+1))
 
     with open('data/'+file_id+'.txt', 'w') as file:
